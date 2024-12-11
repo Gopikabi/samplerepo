7a22558f005651ae022a045d64b91a4cd1d0a37d
sgopi <sgopi@GOPIKA_ABISHEK>
2024-12-11 10:28:13 +0530

    initial commit

---
+++ samplemaven/src/main/java/Sampleinheritence/SingleChild.java
@@ -0,0 +1,16 @@
+package Sampleinheritence;
+
+public class SingleChild extends SingleInheritence {
+
+	public static void main(String[] args) {
+		SingleChild ch=new SingleChild();
+		ch.display();
+		ch.display2();
+
+	}
+	public void display2()
+	{
+		System.out.println("This is the child class");
+	}
+
+}
---
+++ samplemaven/src/main/java/Sampleinheritence/SingleInheritence.java
@@ -0,0 +1,11 @@
+package Sampleinheritence;
+
+public class SingleInheritence {
+
+	public void display()
+	{
+		System.out.println("This is the parent class");
+		
+	}
+}
+
